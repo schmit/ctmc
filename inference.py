c54666825e824b30a4fa228b78bafe6ce726b743
Sven Schmit <schmit@stanford.edu>
2016-03-14 15:16:54 -0700

    negative log-likelihood

--- inference.py
+++ inference.py
@@ -1,1 +1,25 @@
+import numpy as np
+from markovc import *
+
+def nll(x, data):
+    """
+    Negative log-likelihood of x given data
+    """
+    Q = create_rate_matrix_from_vector(x)
+    return sum(nll_term(Q, winner, choices, count) 
+               for (winner, choices), count in data.items())
+
+def nll_term(Q, winner, choices, count):
+    """
+    Single term of log-likelihood of xhat
+    """
+
+    # optimize; can we do this in one step
+    Qs = submatrix(Q, choices)
+
+    winner_idx = [idx for idx, val in enumerate(choices) if winner==val][0]
+
+    piS = equi_ctmc(Qs)[winner_idx]
+    return np.log(np.abs(piS))
+
 
